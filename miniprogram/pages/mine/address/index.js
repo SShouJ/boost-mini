8e6c353a5e8c0be77f9084b85fdca5cbffe400cf
lzp <885387491@qq.com>
2023-02-02 21:54:32 +0800

    收货地址写了一个空状态以及写了一个弹层

--- miniprogram/pages/mine/address/index.js
+++ miniprogram/pages/mine/address/index.js
@@ -5,8 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isOpenNull: true,
+    isOpenDialog: false,
   },
+  // 添加地址的方法
+  addAddress() {
+    console.log('点击了添加地址')
+    // 打开弹层
+    this.isOpen(true);
+  },
+  // 是否打开弹层 传布尔值 true打开 反之关闭
+  isOpen(isok) {
+    this.setData({
+      isOpenDialog: isok,
+    })
+  },
+  // 弹层 确定 按钮的方法
+  determine() {
+    console.log('点击了确认按钮');
+    this.isOpen(false)
+   },
+  // 弹层 取消 按钮的方法
+  cancel() {
+    // 关闭弹层
+    this.isOpen(false)
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
