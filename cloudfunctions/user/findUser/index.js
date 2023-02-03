501d4ea606eb3a73cc87e4e75cb3f5ba8bd64ed3
whs <2014873253@qq.com>
2023-02-03 11:40:41 +0800

    增加了登录接口

---
+++ cloudfunctions/user/findUser/index.js
@@ -0,0 +1,31 @@
+const cloud = require('wx-server-sdk');
+const addUser = require("../addUser/index");
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+});
+
+const db = cloud.database();
+// 获取openId云函数入口函数
+exports.main = async (event, context) => {
+  // 获取基础信息
+  const wxContext = cloud.getWXContext();
+  const openid = wxContext.OPENID;
+  let res = await db.collection('user').where({
+    'openid':openid,
+  }).get();
+  if(res.data.length){
+    return {
+      status:1,
+      msg:'success',
+      data:res.data[0]
+    }
+  }else{
+    await addUser.main(event,context);
+    return {
+      status:0,
+      msg:'查询不到用户',
+      data:[],
+    }
+  }
+  return {};
+};
---
+++ cloudfunctions/user/index.js
@@ -0,0 +1,15 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const findUser = require('./findUser/index');
+const addUser = require('./addUser/index');
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  switch (event.type) {
+    case 'findUser':
+      return await findUser.main(event, context);
+    case "addUser":
+      return await addUser.main(event,context);
+  }
+}
