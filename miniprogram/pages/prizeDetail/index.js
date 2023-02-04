7f65ac08417682f202c6e9bb31893d0c1cbac643
lzp <885387491@qq.com>
2023-02-04 15:28:59 +0800

    完成了奖品详情的静态页面以及部分逻辑

--- miniprogram/pages/prizeDetail/index.js
+++ miniprogram/pages/prizeDetail/index.js
@@ -5,21 +5,150 @@ Page({
    * 页面的初始数据
    */
   data: {
-    parentPass: ''
+    detailData: {},
+    // swiperPrize  prizeLift 这两个数据需要换成接口返回的数据
+    swiperPrize: [
+      {
+        id: 1,
+        url: '../../images/goods1.png',
+        title: '1这是轮播图的标题11111111',
+        day: '111',
+        date: '23:51:31',
+        integral: 100,
+        nav: 'prizeDetail',
+      },
+      {
+        id: 2,
+        url: '../../images/goods1.png',
+        title: '2这是轮播图的标题11111111',
+        day: '222',
+        date: '23:52:31',
+        integral: 200,
+        nav: 'prizeDetail',
+      },
+      {
+        id: 3,
+        url: '../../images/goods1.png',
+        title: '3这是轮播图的标题11111111',
+        day: '333',
+        date: '23:53:31',
+        integral: 300,
+        nav: 'prizeDetail',
+      },
+      {
+        id: 4,
+        url: '../../images/goods1.png',
+        title: '4这是轮播图的标题11111111',
+        day: '444',
+        date: '23:54:31',
+        integral: 400,
+        nav: 'prizeDetail',
+      },
+      {
+        id: 5,
+        url: '../../images/goods1.png',
+        title: '这是轮播图的标题11111111',
+        day: '555',
+        date: '23:55:31',
+        integral: 500,
+        nav: 'prizeDetail',
+      },
+      {
+        id: 6,
+        url: '../../images/goods1.png',
+        title: '6这是轮播图的标题11111111',
+        day: '666',
+        date: '23:56:31',
+        integral: 600,
+        nav: 'prizeDetail',
+      },
+      {
+        id: 7,
+        url: '../../images/goods1.png',
+        title: '7这是轮播图的标题11111111',
+        day: '777',
+        date: '23:57:31',
+        integral: 700,
+        nav: 'prizeDetail',
+      },
+      {
+        id: 8,
+        url: '../../images/goods1.png',
+        title: '8这是轮播图的标题11111111',
+        day: '888',
+        date: '23:58:31',
+        integral: 800,
+        nav: 'prizeDetail',
+      },
+    ],
+    prizeLift: [
+      {
+        id: 0,
+        url: '../../images/price.png',
+        title: '1这是奖品列表的数据',
+        integral: 100,
+        nav: 'prizeDetail',
+      },
+      {
+        id: 1,
+        url: '../../images/price.png',
+        title: '2这是奖品列表的数据',
+        integral: 200,
+        nav: 'prizeDetail',
+      },
+      {
+        id: 2,
+        url: '../../images/price.png',
+        title: '3这是奖品列表的数据',
+        integral: 300,
+        nav: 'prizeDetail',
+      },
+      {
+        id: 3,
+        url: '../../images/price.png',
+        title: '4这是奖品列表的数据',
+        integral: 400,
+        nav: 'prizeDetail',
+      },
+      {
+        id: 4,
+        url: '../../images/price.png',
+        title: '5这是奖品列表的数据',
+        integral: 500,
+        nav: 'prizeDetail',
+      },
+      {
+        id: 5,
+        url: '../../images/price.png',
+        title: '6这是奖品列表的数据',
+        integral: 600,
+        nav: 'prizeDetail',
+      },
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    console.log('传过来的id：', options._identification)
-    this.set_data('parentPass', options._identification)
+    let dataName = options._identification.split(',')[0];
+    let dataId = options._identification.split(',')[1];
+    console.log(dataName + ' --- ', dataId);
+    if (dataName == 'swiper') {
+      this.pushData(this.data.swiperPrize, dataId)
+    } else {
+      this.pushData(this.data.prizeLift, dataId)
+    }
   },
-
-  set_data(key, value) {
-    let data = {}
-    data[key] = value;
-    this.setData(data)
+  pushData(data, id) {
+    data.forEach(item => {
+      if (item.id == id) {
+        this.setData({
+          detailData: { ...item }
+        })
+      }
+    });
+    console.log(this.data.detailData)
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
