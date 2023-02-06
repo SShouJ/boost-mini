a49af68c0976a552b21d907d949c3777d1007afb
whs <2014873253@qq.com>
2023-02-06 21:32:54 +0800

    增加了上传图片的功能

--- miniprogram/pages/mine/settled/index.js
+++ miniprogram/pages/mine/settled/index.js
@@ -5,11 +5,56 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    businessLicense:'',
+    otherImage:'',
   },
   updata(){
     console.log(1);
   },
+ chooseImg(){
+   let _this = this;
+   wx.chooseMedia({
+     count:1,
+     mediaType:['image'],
+     sourceType:['album'],
+     success:function(res){
+       console.log('---------完成上传图片------')
+      console.log(res.tempFiles);
+      wx.cloud.uploadFile({
+        cloudPath: new Date().getTime()+'.png',
+        filePath: res.tempFiles[0].tempFilePath, // 文件路径
+      }).then(res => {
+        // get resource ID
+        console.log(res.fileID)
+      }).catch(error => {
+        // handle error
+      })
+       _this.setData({
+          businessLicense:res.tempFiles[0].tempFilePath
+       })
+     },
+     fail:function(res){
+       console.log(res);
+     }
+   })
+ },
+ chooseImg2(){
+   let _this = this;
+   wx.chooseMedia({
+     count:1,
+     mediaType:['image'],
+     sourceType:['album'],
+     success:function(res){
+       console.log(res.tempFiles[0].tempFilePath);
+       _this.setData({
+          otherImage:res.tempFiles[0].tempFilePath
+       })
+     },
+     fail:function(res){
+       console.log(res);
+     }
+   })
+ },
   /**
    * 生命周期函数--监听页面加载
    */
