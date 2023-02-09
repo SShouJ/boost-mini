f1ec3d2c43d1de43b69a19ec1b3b3ca781a05141
whs <2014873253@qq.com>
2023-02-09 11:15:47 +0800

    增加了审核的接口

--- cloudfunctions/shop/updateProcess/index.js
+++ cloudfunctions/shop/updateProcess/index.js
@@ -1,21 +1,30 @@
 const cloud = require('wx-server-sdk');
-
+const getOneProcess = require('../getOneProcess/index');
+const createShop = require('../createShop/index');
 cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV
 });
 const db = cloud.database()
-
 // 获取openId云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext();
-  const openid = wxContext.OPENID;
   let { id , msg , status} = event;
+  let condition = {};
   // status 1 通过  status 2 不通过  status 0 待审核
-  if(status == 2 && !msg){
-      return {
-        status:0,
-        msg:'拒绝通过原因不能为空',
-        data:[],
+  switch (status) {
+    case 1 :
+      condition = { type : status};
+    case 2 :
+      if(!msg){
+        return {
+          status:0,
+          msg:'拒绝原因不能为空',
+          data:[],
+        }
+      }else{
+        condition = {
+          type:status,
+          msg:msg,
+        }
       }
   }
   //审核
@@ -23,11 +32,13 @@ exports.main = async (event, context) => {
     await db.collection('process').where({
       _id:id,
     }).update({
-      data:{
-        type:status,
-        msg,
-      }
+      data:condition
     })
+    if(status == 1){
+      let res = await getOneProcess(id);
+      console.log('--------------这个是获取一个process数据-----------------');
+      console.log(res);
+    }
     return {
       status:1,
       msg:'success',
@@ -37,7 +48,7 @@ exports.main = async (event, context) => {
     return {
       status:0,
       msg:'fail',
-      data:[],
+      data:error,
     }
   }
 };
