4d6f00e5e90c242a1aff1e3e4918cab32e7c765c
whs <2014873253@qq.com>
2023-02-11 16:02:40 +0800

    增加了每日刷新状态的东西

--- cloudfunctions/activity/changeActivityType/index.js
+++ cloudfunctions/activity/changeActivityType/index.js
@@ -1,9 +1,31 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
+const _ = db.command
 
 cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
 const db = cloud.database();
 // 云函数入口函数
 exports.main = async (event, context) => {
     let nowTime = Date.now();
+    await db.collection('activity').where({
+      start:_.lt(nowTime)
+    }).update({
+      data:{
+        type:4
+      }
+    })//未开始
+    await db.collection('activity').where({
+      start:_.gt(nowTime)
+    }).update({
+      data:{
+        type:2,
+      }
+    })//进行中
+    await db.collection('activity').where({
+      end:_.gt(nowTime)
+    }).update({
+      data:{
+        type:3
+      }
+    })
 }
