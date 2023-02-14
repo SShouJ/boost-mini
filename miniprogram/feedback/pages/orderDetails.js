a637fae2b80c475c0070993e6ca7b5eb69dad941
qmzzzzzz <2772432309@qq.com>
2023-02-14 11:31:33 +0800

    完成了订单详情的静态页面

---
+++ miniprogram/feedback/pages/orderDetails.js
@@ -0,0 +1,75 @@
+// feedback/pages/orderDetails.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    detailsList:[{
+      name:'刘将军',
+      time:'2023年12月11日23:22',
+      businessLicense:'../../images/img-businesslicense.png',
+      shopName:'小刘酒吧',
+      shopAddress:'卫辉市比干大道',
+      contacts:'刘将军',
+      contactsTelephone:'18203477587',
+      briefIntroduction:'666'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
