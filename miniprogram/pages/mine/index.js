012669d6f645170f48a6c8458024600c45778fe9
ssj <2226435102@qq.com>
2023-02-15 16:29:03 +0800

    登录方法

--- miniprogram/pages/mine/index.js
+++ miniprogram/pages/mine/index.js
@@ -1,4 +1,3 @@
-
 // pages/mine/index.js
 Page({
 
@@ -8,95 +7,127 @@ Page({
   data: {
     islogin: false,
     userInfo: {},
-    actInfoList:[
-      {
-        id:1,
-        number:10,
-        acttext:'已参与活动',
-        page:"attendEvents"
+    actInfoList: [{
+      id: 1,
+      number: 10,
+      acttext: '已参与活动',
+      page: "attendEvents"
 
+    }, {
+      id: 2,
+      number: 0,
+      acttext: '中奖记录',
+      page: "winningRecords"
+    }, {
+      id: 3,
+      number: 0,
+      acttext: '兑换记录',
+      page: "redemptionHistory"
+    }],
+    mineNav: [{
+        id: 1,
+        title: "我的活动",
+        image: "../../images/icon-myactive.png",
+        name: "myActive"
+      },
+      {
+        id: 2,
+        title: "商家入驻",
+        image: "../../images/icon-settled.png",
+        name: "settled"
+      },
+      {
+        id: 3,
+        title: "入驻审核",
+        image: "../../images/icon-audit.png",
+        name: "audit"
       }, {
-        id:2,
-        number:0,
-        acttext:'中奖记录',
-        page:"winningRecords"
-      },{
-        id:3,
-        number:0,
-        acttext:'兑换记录',
-        page:"redemptionHistory"
+        id: 4,
+        title: "收货地址",
+        image: "../../images/icon-audit.png",
+        name: "address"
       }
-    ],
-   mineNav:[
-     {
-      id:1,
-      title:"我的活动",
-      image:"../../images/icon-myactive.png",
-      name:"myActive"
-    },
-    {
-      id:2,
-      title:"商家入驻",
-      image:"../../images/icon-settled.png",
-      name:"settled"
-    },
-    {
-      id:3,
-      title:"入驻审核",
-      image:"../../images/icon-audit.png",
-      name:"audit"
-    },{
-      id:4,
-      title:"收货地址",
-      image:"../../images/icon-audit.png",
-      name:"address"
-    }
-   ]
+    ]
   },
-  toPage(data){
+  toPage(data) {
+    let _this = this
     console.log(data.currentTarget.dataset.name);
-    wx.navigateTo({
-      url: '/pages/mine/'+ data.currentTarget.dataset.name+'/index',
-    })
-  },
-  toFeedbackPages(data){
-    console.log(data.currentTarget.dataset.name);
-    wx.navigateTo({
-      url: '/feedback/pages/'+ data.currentTarget.dataset.name,
-    })
-  },
- async addUser(){
-    let res = await getApp().addUserInfo()
-    if (res.result.status == 1 ) {
-        let userInfo = await getApp().getUserInfo()
-        this.setData({
-          userInfo : res.result.data,
-          islogin : true
-          })
+    if (this.data.userInfo.openid) {
+
+      wx.navigateTo({
+        url: '/pages/mine/' + data.currentTarget.dataset.name + '/index',
+      })
+    }else{
+      _this.addUser()
     }
-    console.log(this.data.userInfo);
- },
-  
+  },
+  toFeedbackPages(data) {
+    let _this = this
+    console.log(data.currentTarget.dataset.name);
+    if (this.data.userInfo.openid) {
+      wx.navigateTo({
+        url: '/feedback/pages/' + data.currentTarget.dataset.name,
+      })
+    } else {
+      _this.addUser()
+    }
+  },
+  async addUser() {
+    console.log(1);
+    let {
+      getUserInfo,
+      addUserInfo,
+      getUserProfile
+    } = getApp();
+    let res = await getUserProfile();
+    console.log(res);
+    let res2 = await addUserInfo();
+    console.log(res2);
+    if (res2.result.status == 1) {
+      let res3 = await getUserInfo();
+      console.log(res3);
+      this.setData({
+        userInfo: res3.result.data,
+        islogin: true
+      })
+    }
+    // console.log('同意');
+    // wx.getUserProfile({
+    //   desc: '用于登录',
+    //   success: async(res) => {
+    //     let res = await getApp().addUserInfo()
+    //     console.log(res);
+    //   }
+    // })
+    //   if (res.result.status == 1 ) {
+    //       let userInfo = await getApp().getUserInfo();
+    //       this.setData({
+    //         userInfo : userInfo,
+    //         islogin : true
+    //         })
+    //   }
+    //   console.log(this.data.userInfo);
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   async onLoad(options) {
-    let res= await getApp().getUserInfo()
+    let res = await getApp().getUserInfo()
     if (res.result.status == 1) {
       this.setData({
-      userInfo : res.result.data,
-      islogin : true
+        userInfo: res.result.data,
+        islogin: true
       })
-    }else{
-        console.log(res.result.msg);
+    } else {
+      console.log(res.result.msg);
     }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
@@ -140,4 +171,3 @@ Page({
 
   }
 })
-
