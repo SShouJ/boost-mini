690c5af5d93ace12ba0ca5d86f88baa88a4206a6
whs <2014873253@qq.com>
2023-02-16 15:06:30 +0800

    增加了获取爱好的接口

--- cloudfunctions/good/index.js
+++ cloudfunctions/good/index.js
@@ -2,10 +2,13 @@
 const cloud = require('wx-server-sdk')
 cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
 const getCategoryList = require('./getCategoryList/index');
+const getGoodByCategory = require('./getGoodByCategory/index');
 // 云函数入口函数
 exports.main = async (event, context) => {
   switch (event.type) {
     case 'getCategoryList':
       return await getCategoryList.main(event, context);
+    case 'getGoodByCategory':
+      return await getGoodByCategory.main(event,context);
   }
 }
