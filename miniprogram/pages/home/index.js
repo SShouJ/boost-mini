9f98102fcb9dc678f80dfa63a4f126359df027ff
lzp <885387491@qq.com>
2023-02-17 15:42:38 +0800

    完善了首页数据接口的调试

--- miniprogram/pages/home/index.js
+++ miniprogram/pages/home/index.js
@@ -12,44 +12,44 @@ Page({
     selected: [],
     userInfo: null,
     isOpenDialog: false,
-    isDisplay: false
+    isDisplay: false,
+    showPrize: true,
   },
   // 判断是否登录了
-  isLogin() {
+  async isLogin() {
     /**
      * 若登录 则展示正常布局页面 弹层啥的
      * 若未登录  则在header模块展示一个 未登录 的标题，并关闭让用户选择爱好的弹层 
      */
-    wx.cloud.callFunction({
+    let res = await wx.cloud.callFunction({
       name: 'user',
       data: {
         type: "getUserInfo",
       }
-    }).then(res => {
-      if (res.result.status == 1 && res.result.data) {
-        console.log('userinfo', res.result.data);
-        if (res.result.data.hobbys.length == 0) {
-          // 打开爱好弹层
-          this.isOpen(true);
-        }
-        // 设置 用户信息的数据给 userinfo   设置
-        this.setData({
-          userInfo: res.result.data,
-          isDisplay: true
-        })
-      } else {
-        // 先关闭让用户选择爱好的弹层
-        // 不管有没有数据 当掉完接口的时候 都要关闭加载中放开页面显示限制 
-        this.isOpen(false);
-        this.setData({
-          isDisplay: true
-        })
-      }
-      if ((this.data.userInfo && this.data.prizeLift.length) || (this.data.hobbys.length && this.data.prizeLift.length)) {
-        // 加载中的这个动画  要么用户登录成功 并且所有奖品的数据都拿到了才关闭   要么就  爱好列表的数据和奖品列表的数据全拿到的时候 也关闭
-        wx.hideToast()
-      }
     })
+    if (res.result.status == 1 && res.result.data) {
+      if (res.result.data.hobbys.length == 0) {
+        // 打开爱好弹层
+        this.isOpen(true);
+      } else {
+        this.accordingToHobbiesGetCommodity(res.result.data.hobbys);
+      }
+      // 设置 用户信息的数据给 userinfo   设置
+      this.setData({
+        userInfo: res.result.data,
+        isDisplay: true
+      })
+    } else {
+      // 先关闭让用户选择爱好的弹层
+      // 不管有没有数据 当掉完接口的时候 都要关闭加载中放开页面显示限制 
+      this.getAllGoodByCategory()
+      this.isOpen(false);
+      this.setData({
+        isDisplay: true
+      })
+    }
+
+    wx.hideToast()
   },
   // 是否打开弹层 传布尔值 true打开 反之关闭
   isOpen(isok) {
@@ -95,8 +95,13 @@ Page({
   },
   // changeItem 是监测tab列表当前选中的是哪个的方法
   changeItem(e) {
+    wx.showToast({
+      title: '加载中...',
+      icon: 'loading',
+    });
     this.setData({
       target: e.currentTarget.dataset.id,
+      showPrize: false,
     })
     let title = ''
     this.data.navList.forEach((item, i) => {
@@ -104,173 +109,19 @@ Page({
         title = item.name
       }
     })
-    wx.showToast({
-      title: '加载中...',
-      icon: 'loading',
-    });
-    console.log('当前选中的是第', this.data.target, '个tab列表; title：', title);
-    let navId = this.data.navList[this.data.target]._id
+    let navId = this.data.navList[this.data.target]._id;
     if (this.data.target == 0) {
-      // 若类目的 id 为 0 则调用所有商品的接口
-      this.getAllGoodByCategory()
+      if (this.data.userInfo.hobbys) {
+        this.accordingToHobbiesGetCommodity(this.data.userInfo.hobbys);
+      } else {
+        // 若类目的 id 为 0 且没有选择爱好则调用所有商品的接口
+        this.getAllGoodByCategory()
+      }
     } else {
       this.getGoodByCategory(navId)
     }
   },
-  // getPrizeList 是获取奖品列表数据的方法
-  // getPrizeList(type) {
-  //   // prizeLift1 prizeLift2 prizeLift3 prizeLift4 这几个等接口出来要分别改成一个对应的 掉接口的方法
-  //   let prizeLift1 = [
-  //     {
-  //       id: 0,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '1这是奖品列表的数据',
-  //       integral: 100,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 1,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '2这是奖品列表的数据',
-  //       integral: 200,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 2,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '3这是奖品列表的数据',
-  //       integral: 300,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 3,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '4这是奖品列表的数据',
-  //       integral: 400,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 4,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '5这是奖品列表的数据',
-  //       integral: 500,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 5,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '6这是奖品列表的数据',
-  //       integral: 600,
-  //       nav: 'prizeDetail',
-  //     },
-  //   ];
-  //   let prizeLift2 = [
-  //     {
-  //       id: 0,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '1这是分类2里的数据',
-  //       integral: 100,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 1,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '2这是分类2里的数据',
-  //       integral: 200,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 2,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '3这是分类2里的数据',
-  //       integral: 300,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 3,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '4这是分类2里的数据',
-  //       integral: 400,
-  //       nav: 'prizeDetail',
-  //     },
-  //   ];
-  //   let prizeLift3 = [
-  //     {
-  //       id: 0,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '1分类三的数据',
-  //       integral: 100,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 1,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '2分类三的数据',
-  //       integral: 200,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 2,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '3分类三的数据',
-  //       integral: 300,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 3,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '4分类三的数据',
-  //       integral: 400,
-  //       nav: 'prizeDetail',
-  //     },
-  //   ];
-  //   let prizeLift4 = [
-  //     {
-  //       id: 0,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '1分类四的数据',
-  //       integral: 100,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 1,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '2分类四的数据',
-  //       integral: 200,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 2,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '3分类四的数据',
-  //       integral: 300,
-  //       nav: 'prizeDetail',
-  //     },
-  //     {
-  //       id: 3,
-  //       url: 'https://636c-cloud1-7ge7nl2m42cee9e9-1316264853.tcb.qcloud.la/goods1.png?sign=a05f2c0805a974e22e8be43c4f4d5c40&t=1675763308',
-  //       title: '4分类四的数据',
-  //       integral: 400,
-  //       nav: 'prizeDetail',
-  //     },
-  //   ];
-  //   let data = null
-  //   if (type == 1) {
-  //     data = prizeLift1
-  //   } else if (type == 2) {
-  //     data = prizeLift2
-  //   } else if (type == 3) {
-  //     data = prizeLift3
-  //   } else if (type == 4) {
-  //     data = prizeLift4
-  //   }
-  //   // this.setData({
-  //   //   prizeLift: data
-  //   // })
-  //   console.log('当前页的数据是', this.data.prizeLift);
-  // },
   // prizeLiftNav 跳转详情页面 + 传参 的方法
-
   prizeLiftNav(data) {
     let item = data.currentTarget.dataset.name;
     wx.navigateTo({
@@ -308,7 +159,6 @@ Page({
             name: '推荐'
           },
         ]
-        console.log('类目', navData)
         res.result.data.data.forEach((e) => {
           navData.push(e);
         })
@@ -325,46 +175,48 @@ Page({
       name: 'good',
       data: {
         type: 'getGoodByCategory',
-        category: id
+        category: id,
+        pageSize: 4,
       }
     })
     if (res.result.data.data.length) {
-      console.log('数据为：', res.result.data.data);
       this.setData({
-        prizeLift: res.result.data.data
+        prizeLift: res.result.data.data,
+        showPrize: true,
       })
       wx.hideToast()
     }
   },
-  // 获取所有商品
+  // 获取所有商品接口  
   async getAllGoodByCategory() {
-    // 默认初始状态掉一次获取商品的接口
     let res = await wx.cloud.callFunction({
       name: 'good',
       data: {
         type: 'getGoodByCategory',
+        pageSize: 4,
       }
     })
     if (res.result.data.data.length) {
-      console.log('数据为：', res.result.data.data);
       this.setData({
-        prizeLift: res.result.data.data
+        prizeLift: res.result.data.data,
+        showPrize: true,
       })
       this.hideToast();
     }
   },
   // 设置用户爱好
-  setUserHobby(hobbys) {
-    wx.cloud.callFunction({
+  async setUserHobby(hobbys) {
+    let res = await wx.cloud.callFunction({
       name: 'user',
       data: {
         type: "updateUser",
         hobbys,
       }
-    }).then(res => {
-      console.log('设置爱好接口返回的：', res)
     })
-
+    if(res){
+      console.log(res)
+    }
+    this.isLogin();
     if (hobbys) {
       wx.showToast({
         title: '选择成功！',
@@ -378,6 +230,24 @@ Page({
         icon: 'error',//icon
         duration: 1500 //停留时间
       })
+    }
+  },
+  // 根据爱好获取商品的接口
+  async accordingToHobbiesGetCommodity(data) {
+    let res = await wx.cloud.callFunction({
+      name: 'good',
+      data: {
+        type: "getGoodByCategory",
+        hobbys: data,
+        pageSize: 4
+      }
+    })
+    if (res.result.status == 1) {
+      this.setData({
+        showPrize:true,
+        prizeLift: res.result.data.data
+      })
+      wx.hideToast()
     }
   },
   // 监测是否满足关闭加载动画
@@ -394,8 +264,6 @@ Page({
     this.isOpen(false);
     // 调用获取类目的接口  给爱好以及tab切换的title赋值
     this.getClassList()
-    // 初始状态调用一次获取所有商品的接口
-    this.getAllGoodByCategory()
   },
 
   /**
@@ -409,7 +277,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-    console.log('当前选中的是第', this.data.target, '个tab列表;title：', '推荐');
     // this.getPrizeList(this.data.target);
     this.setData({
       isDisplay: false,
