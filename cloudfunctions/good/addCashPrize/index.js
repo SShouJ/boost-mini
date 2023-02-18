f0b91418bd47ab23d90a5b2b8d15ca7ad2d5fa19
whs <2014873253@qq.com>
2023-02-18 10:07:02 +0800

    测试兑换积分接口

--- cloudfunctions/good/addCashPrize/index.js
+++ cloudfunctions/good/addCashPrize/index.js
@@ -11,6 +11,8 @@ exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext();
   const openid = wxContext.OPENID;//用户的open_id;
   let { prizeId } = event;//奖品id
+  let nowTime =  Date.now(); //开始时间
+  let endTime = nowTime + 86400000 * 7;
   const result = await db.runTransaction(async transaction => {
     try {
       //首先获取用户是否存在, 再是获取奖品是否存在，然后就是判断积分是否足够，够的话，直接减少积分，并更改倒用户表
@@ -32,6 +34,12 @@ exports.main = async (event, context) => {
           data:[],
         }
       }
+      console.log('-------------------这个是检查积分是否足够--------------------');
+      console.log('------这个是用户积分---');
+      console.log(userRes.data.integralNum);
+      console.log('-----这个是商品的需要的积分---');
+      console.log(goodRes.data.integral);
+      console.log('------------------------------------------------------------');
       if(userRes.data.integralNum < goodRes.data.integral){
         return {
           status:0,
@@ -56,6 +64,9 @@ exports.main = async (event, context) => {
         data: {
           prizeId,
           userId:openid,
+          start:nowTime,
+          end:endTime,
+          address:'乌拉拉工作室'
         }
       });
       return {
@@ -65,7 +76,6 @@ exports.main = async (event, context) => {
       }
     } catch (e) {
       console.log('事务报错'+e);
-      transaction.rollback(-100)
       await transaction.rollback({
         status: 0,
         msg: e,
